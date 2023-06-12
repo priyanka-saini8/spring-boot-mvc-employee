ab35dbf358ed68f97edacdc80efb463f5f563da5
priyanka-saini8 <63971975+priyanka-saini8@users.noreply.github.com>
2023-06-13 00:42:24 +0530

    add new employee

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -55,6 +57,24 @@ public class EmployeeController {
 		theModel.addAttribute("employees", theEmployees);
 
 		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+
+		Employee theEmployee = new Employee();
+
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
 	}
 }
 
