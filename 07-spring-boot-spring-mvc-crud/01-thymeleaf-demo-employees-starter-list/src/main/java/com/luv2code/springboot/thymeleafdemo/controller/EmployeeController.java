6a50479b8533c3e5cc8467077b314355a8996079
priyanka-saini8 <63971975+priyanka-saini8@users.noreply.github.com>
2023-06-12 23:28:45 +0530

    basic

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,36 +16,45 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
+	private EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
+
+	//	// load employee data
+//
+//	private List<Employee> theEmployees;
+//
+//	@PostConstruct
+//	private void loadData() {
+//
+//		// create employees
+//		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
+//		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
+//		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
+//
+//		// create the list
+//		theEmployees = new ArrayList<>();
+//
+//		// add to the list
+//		theEmployees.add(emp1);
+//		theEmployees.add(emp2);
+//		theEmployees.add(emp3);
+//	}
 
 	// add mapping for "/list"
 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
+		List<Employee> theEmployees = employeeService.findAll();
+
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
 	}
 }
 
